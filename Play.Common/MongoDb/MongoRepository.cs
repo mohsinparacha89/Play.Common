4cf1504e0281434ef5b9fda9224d7d0fc499fffd
mohsin.paracha88 <mohsin.paracha88@gmail.com>
2021-06-26 23:58:17 +0800

    some stuff

---
+++ Play.Common/MongoDb/MongoRepository.cs
@@ -0,0 +1,71 @@
+﻿using MongoDB.Driver;
+using Play.Common.Entities;
+using Play.Common.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Play.Common.MongoDb
+{
+    public class MongoRepository<T> : IRepository<T> where T : IEntity
+    {
+
+        private readonly IMongoCollection<T> dbCollection;
+        private readonly FilterDefinitionBuilder<T> filterBuilder = Builders<T>.Filter;
+
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            dbCollection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(e => e.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity was null...");
+            }
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity was null...");
+            }
+
+            FilterDefinition<T> filter = filterBuilder.Eq(e => e.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(e => e.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
+
+    }
+}
---
+++ Play.Common/MongoDb/ServiceExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Common.Entities;
+using Play.Common.Settings;
+using Play.Common.Repositories;
+using Play.Common.MongoDb;
+
+namespace Play.Common.MongoDb
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            services.AddSingleton(sp => {
+
+                var configuration = sp.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var mongoDbSettings = configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            });
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collectionName) where T : IEntity
+        {
+            services.AddSingleton<IRepository<T>>(sp => {
+                var db = sp.GetService<IMongoDatabase>();
+                return new MongoRepository<T>(db, collectionName);
+            });
+
+            return services;
+        }
+
+
+    }
+}
